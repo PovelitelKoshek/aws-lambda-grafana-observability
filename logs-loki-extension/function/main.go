9a999be3f8ee33a0073337388495c02dddec33af
PovelitelKoshek <savenko.stepan.aleks@gmail.com>
2026-06-01 12:40:52 +0300

    Update main.go

--- logs-loki-extension/function/main.go
+++ logs-loki-extension/function/main.go
@@ -11,13 +11,13 @@ import (
 type Event map[string]interface{}
 
 func handler(ctx context.Context, event Event) (map[string]interface{}, error) {
-	log.Println("TEST3: Lambda function started")
-	log.Printf("TEST3: incoming event = %+v", event)
-	log.Println("TEST3: this log should go to Grafana Cloud Loki through Lambda Extension")
+	log.Println("TEST: Lambda function started")
+	log.Printf("TEST: incoming event = %+v", event)
+	log.Println("TEST: this log should go to Grafana Cloud Loki through Lambda Extension")
 
 	return map[string]interface{}{
 		"status":  "ok",
-		"message": "Hello from Test3 Lambda",
+		"message": "Hello from Test Lambda",
 		"time":    time.Now().Format(time.RFC3339),
 	}, nil
 }
